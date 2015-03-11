d5cfef0f2d022e20d71038dd879ddc2dce7f3a73
Anders Abel <anders@abel.nu>
2015-03-11 19:25:27 +0100

    Added MVC Application

---
+++ MVCApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCApplication.Startup))]
+namespace MVCApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
