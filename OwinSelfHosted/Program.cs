4c5965863c8e24cb6efd21d7c5c5825be76fa623
Anders Abel <anders@abel.nu>
2015-03-11 13:16:00 +0100

    Created empty self hosted application.

--- OwinSelfHosted/Program.cs
+++ OwinSelfHosted/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,23 @@ namespace OwinSelfHosted
     {
         static void Main(string[] args)
         {
+            string url = "http://localhost:12345";
+            Console.WriteLine("Starting web application...");
+            using(WebApp.Start(url, Startup))
+            {
+                Console.WriteLine("Application listening on {0}.", url);
+                Console.WriteLine("Press any key to quit.");
+
+                Console.ReadKey();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Application shut down.");
+        }
+
+        static void Startup(IAppBuilder app)
+        {
+
         }
     }
 }
